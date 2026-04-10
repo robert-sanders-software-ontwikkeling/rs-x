a61e540313ed78dda48026fc10d35714075c0a67
robert-sanders-software-ontwikkeling <robert.sanders.software@outlook.com>
2026-04-10 15:55:24 +0200

    Update WCAG tests

--- rs-x-site/playwright.config.ts
+++ rs-x-site/playwright.config.ts
@@ -16,8 +16,12 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'chromium-light',
+      use: { ...devices['Desktop Chrome'], colorScheme: 'light' },
+    },
+    {
+      name: 'chromium-dark',
+      use: { ...devices['Desktop Chrome'], colorScheme: 'dark' },
     },
   ],
 
